5e793f960472006f160678fd38040336018d67b1
Kaid22 <avdickaid@gmail.com>
2021-03-14 18:21:38 +0100

    komita

--- skokovi/Form1.cs
+++ skokovi/Form1.cs
@@ -16,5 +16,54 @@ namespace skokovi
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s1 = int.Parse(textBox1.Text);
+            int s2 = int.Parse(textBox4.Text);
+            int m1 = int.Parse(textBox2.Text);
+            int m2 = int.Parse(textBox5.Text);
+            int sek1 = int.Parse(textBox3.Text);
+            int sek2 = int.Parse(textBox6.Text);
+            if (s1 > s2)
+            {
+                label3.Text = "alfa veci od beta";
+            }
+            else if (s1 < s2)
+            {
+                label3.Text = "beta veci od alfa";
+            }
+            else if (s1 == s2)
+            {
+                if (m1 > m2)
+                {
+                    label3.Text = "alfa veci od beta";
+                }
+                else if (m1 < m2)
+                {
+                    label3.Text = "beta veci od alfa";
+                }
+                else if (m1 == m2)
+                {
+                    if (sek1 > sek2)
+                    {
+                        label3.Text = "alfa veci od beta";
+                    }
+                    else if (sek1 < sek2)
+                    {
+                        label3.Text = "beta veci od alfa";
+                    }
+                    else if (sek1 == sek2)
+                    {
+                        label3.Text = "uglovi su jednaki";
+                    }
+                }
+            }
+        }
     }
 }
